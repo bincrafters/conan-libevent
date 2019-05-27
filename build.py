b532e0982443872426dd683e9c1cea74c234b215
theirix <theirix@gmail.com>
2019-05-27 16:25:24 +0300

    Reduce number of builds without OpenSSL
    AppVeyor builds will timeout after 1 hour with 6 builds so check
    only essentials configurations

--- build.py
+++ build.py
@@ -2,10 +2,15 @@
 # -*- coding: utf-8 -*-
 from bincrafters import build_template_default
 import platform
+import copy
 
 if __name__ == "__main__":
     builder = build_template_default.get_builder(pure_c=True)
-    if platform.system() == "Windows":
-        for settings, options, env_vars, build_requires, reference in reversed(builder.items):
-            builder.add(settings, {"libevent:with_openssl": False}, env_vars, build_requires)
+    for settings, options, env_vars, build_requires, reference in reversed(builder.items):
+        if settings["build_type"] == "Release" and \
+                (not platform.system() == "Windows" or settings.compiler.runtime == "MD"):
+            # build without OpenSSL for specific builds because Windows builds are very time-limited
+            new_options = copy.copy(options)
+            new_options["libevent:with_openssl"] = False
+            builder.add(settings, new_options, env_vars, build_requires)
     builder.run()
