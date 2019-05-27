6c325ed7356a9004cee7c639d8152c8f97337d66
theirix <theirix@gmail.com>
2019-05-27 11:12:55 +0300

    Version 2.1.10 bump

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ from conans.errors import ConanInvalidConfiguration
 
 class LibeventConan(ConanFile):
     name = "libevent"
-    version = "2.1.8"
+    version = "2.1.10"
     description = "libevent - an event notification library"
     topics = ("conan", "libevent", "event")
     url = "https://github.com/bincrafters/conan-libevent"
