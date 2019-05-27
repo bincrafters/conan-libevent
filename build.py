d2e17a6f66021c384a03a2d33b360aa229eca32c
theirix <theirix@gmail.com>
2019-05-27 17:08:17 +0300

    Fix CPT build condiition

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     builder = build_template_default.get_builder(pure_c=True)
     for settings, options, env_vars, build_requires, reference in reversed(builder.items):
         if settings["build_type"] == "Release" and \
-                (not platform.system() == "Windows" or settings.compiler.runtime == "MD"):
+                (settings["compiler"] != "Visual Studio" or settings["compiler.runtime"] == "MD"):
             # build without OpenSSL for specific builds because Windows builds are very time-limited
             new_options = copy.copy(options)
             new_options["libevent:with_openssl"] = False
