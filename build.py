635c250d019511ed2489917d6be9eba332fa2e8d
theirix <theirix@gmail.com>
2019-05-28 16:06:33 +0300

    Build without openssl on release configurations

--- build.py
+++ build.py
@@ -7,9 +7,7 @@ import copy
 if __name__ == "__main__":
     builder = build_template_default.get_builder(pure_c=True)
     for settings, options, env_vars, build_requires, reference in reversed(builder.items):
-        if settings["build_type"] == "Release" and \
-                (settings["compiler"] != "Visual Studio" or settings["compiler.runtime"] == "MD"):
-            # build without OpenSSL for specific builds because Windows builds are very time-limited
+        if settings["build_type"] == "Release":
             new_options = copy.copy(options)
             new_options["libevent:with_openssl"] = False
             builder.add(settings, new_options, env_vars, build_requires)
